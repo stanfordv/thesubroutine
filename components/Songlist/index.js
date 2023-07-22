539b048effd5d3d9c05449f8a22af3f017d49aa5
stanford <stanfordv@gmail.com>
2023-07-22 21:07:01 +0200

    mp3s running on google cloud now

--- components/Songlist/index.js
+++ components/Songlist/index.js
@@ -5,44 +5,45 @@ function Songlist() {
   const songs = [
     {
       name: "The Machine ",
-      source: "/songs/themachine.mp3",
+      source: "https://storage.googleapis.com/subroutinesongs/themachine.mp3",
       image: "/songs/themachine.jpg",
     },
     {
       name: "Without a sound",
-      source: "/songs/withoutasound.mp3",
+      source:
+        "https://storage.googleapis.com/subroutinesongs/withoutasound.mp3",
       image: "/songs/withoutasound.jpg",
     },
     {
       name: "What I Know",
-      source: "/songs/whatiknow.mp3",
+      source: "https://storage.googleapis.com/subroutinesongs/whatiknow.mp3",
       image: "/songs/whatiknow.jpg",
     },
 
     {
       name: "Busted ",
-      source: "/songs/busted.mp3",
+      source: "https://storage.googleapis.com/subroutinesongs/busted.mp3",
       image: "/songs/busted.jpg",
     },
     {
       name: "Critter Cams",
-      source: "/songs/crittercams.mp3",
+      source: "https://storage.googleapis.com/subroutinesongs/crittercams.mp3",
       image: "/songs/crittercams.png",
     },
     {
       name: "Sunrise",
-      source: "/songs/sunrise.mp3",
+      source: "https://storage.googleapis.com/subroutinesongs/sunrise.mp3",
       image: "/songs/sunrise.jpg",
     },
 
     {
       name: "Machine - Side 1",
-      source: "/songs/machineside1.mp3",
+      source: "https://storage.googleapis.com/subroutinesongs/machineside1.mp3",
       image: "/songs/machine_album.gif",
     },
     {
       name: "Machine - Side 2",
-      source: "/songs/machineside2.mp3",
+      source: "https://storage.googleapis.com/subroutinesongs/machineside2.mp3",
       image: "/songs/machine_album.gif",
     },
   ];
