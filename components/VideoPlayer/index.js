c78b5cbfd72ba1b1535994ce64f5b692f257e60f
stanford <stanfordv@gmail.com>
2023-07-24 22:03:12 +0200

    added video modal

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -1,17 +1,34 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Card, CardBody, CardImg, CardTitle, CardText } from "reactstrap";
 import styles from "@/components/VideoPlayer/videoplayer.module.css";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-import Link from "next/link";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+Modal.setAppElement("#__next");
 
 function VideoPlayer({ videos }) {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [currentVideo, setCurrentVideo] = useState("");
+
+  function openModal(video) {
+    setCurrentVideo(video);
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className={styles.videoplayercontainer}>
       {videos.map((video, index) => (
         <div className={styles.video_container} key={index}>
           <div className={styles.video_left}>
             <div className={styles.video_title}>{video.name}</div>
-            <Link href={video.source} target="_blank">
+            <div onClick={() => openModal(video.source)}>
               <Image
                 className={styles.video_img}
                 src={video.image}
@@ -19,13 +36,40 @@ function VideoPlayer({ videos }) {
                 width={250}
                 height={140}
               />
-            </Link>
+            </div>
           </div>
           <div className={styles.video_right}>
             <div className={styles.video_text}>{video.description}</div>
           </div>
         </div>
       ))}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 33, 71, 0.75)",
+          },
+          content: {
+            background: "black", // Set the background color of the content area
+            color: "var(--color-light)", // Set the color of any text in the content area
+          },
+        }}
+        contentLabel="Example Modal"
+      >
+        <button className={styles.closebtn} onClick={closeModal}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+        <iframe
+          width="100%"
+          height="100%"
+          src={currentVideo}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </Modal>
     </div>
   );
 }
--- pages/videos.js
+++ pages/videos.js
@@ -6,28 +6,32 @@ export default () => {
   const videos = [
     {
       name: "Critter Cams",
-      source: "https://youtu.be/TjZsLNdLNTk",
+      youtube_link: "https://youtu.be/watch?v=TjZsLNdLNTk",
+      source: "https://www.youtube.com/embed/TjZsLNdLNTk",
       image: "/videos/crittercamslink.png",
       description:
         "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       name: "The Machine",
-      source: "https://www.youtube.com/watch?v=-7Ul4D3Rw78",
+      youtube_link: "https://www.youtube.com/watch?v=-7Ul4D3Rw78",
+      source: "https://www.youtube.com/embed/-7Ul4D3Rw78",
       image: "/videos/machinethumb.png",
       description:
         "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       name: "Without a sound",
-      source: "https://www.youtube.com/watch?v=Sq5ZtXGA2pE",
+      youtube_link: "https://www.youtube.com/watch?v=Sq5ZtXGA2pE",
+      source: "https://www.youtube.com/embed/Sq5ZtXGA2pE",
       image: "/videos/withoutasoundyoutubethumb.png",
       description:
         "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       name: "Sway",
-      source: "https://www.youtube.com/watch?v=KHSBaBe5X5g",
+      youtube_link: "https://www.youtube.com/watch?v=KHSBaBe5X5g",
+      source: "https://www.youtube.com/embed/KHSBaBe5X5g",
       image: "/videos/swayyoutubethumb.png",
       description:
         "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
