454fe6079a24a58d046a2006ec90cf06016fdc78
stanford <stanfordv@gmail.com>
2023-08-12 19:36:35 +0200

    no lorem ipsum

--- pages/videos.js
+++ pages/videos.js
@@ -9,32 +9,32 @@ export default () => {
       youtube_link: "https://youtu.be/watch?v=TjZsLNdLNTk",
       source: "https://www.youtube.com/embed/TjZsLNdLNTk",
       image: "/videos/crittercamslink.png",
-      description:
-        // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "",
+      // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       name: "The Machine",
       youtube_link: "https://www.youtube.com/watch?v=-7Ul4D3Rw78",
       source: "https://www.youtube.com/embed/-7Ul4D3Rw78",
       image: "/videos/machinethumb.png",
-      description:
-        // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "",
+      // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       name: "Without a sound",
       youtube_link: "https://www.youtube.com/watch?v=Sq5ZtXGA2pE",
       source: "https://www.youtube.com/embed/Sq5ZtXGA2pE",
       image: "/videos/withoutasoundyoutubethumb.png",
-      description:
-        // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "",
+      // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
       name: "Sway",
       youtube_link: "https://www.youtube.com/watch?v=KHSBaBe5X5g",
       source: "https://www.youtube.com/embed/KHSBaBe5X5g",
       image: "/videos/swayyoutubethumb.png",
-      description:
-        // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "",
+      // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
   ];
   return (
