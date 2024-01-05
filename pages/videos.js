bbb4fb7cdda09b1539715f0149479354ea74e4c0
stanfordv <stanfordv@gmail.com>
2024-01-05 23:13:05 +0100

    Update crittercam link

--- pages/videos.js
+++ pages/videos.js
@@ -6,8 +6,8 @@ export default () => {
   const videos = [
     {
       name: "Critter Cams",
-      youtube_link: "https://youtu.be/watch?v=TjZsLNdLNTk",
-      source: "https://www.youtube.com/embed/TjZsLNdLNTk",
+      youtube_link: "https://youtu.be/watch?v=ZUOE-2lFHek",
+      source: "https://www.youtube.com/embed/ZUOE-2lFHek",
       image: "/videos/crittercamslink.png",
       description: "",
       // "Lorem ipsum dolor sit amet, aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
